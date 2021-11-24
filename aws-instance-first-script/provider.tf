f52cf32e4fde46a19b5eaadefa7255e9af8bf874
para93 <92060430+para93@users.noreply.github.com>
2021-11-23 20:23:39 -0800

    Update provider.tf

--- aws-instance-first-script/provider.tf
+++ aws-instance-first-script/provider.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
-
 provider "aws" {
   region  = "${var.region}"
 }
